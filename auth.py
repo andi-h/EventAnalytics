28f145df7d80a9f8697dc4832be4569fd136bf43
Andreas Hirtenlehner <andreas.hirtenlehner@heap-engineering.at>
2025-08-12 23:13:35 +0200

    fix auth

--- auth.py
+++ auth.py
@@ -16,15 +16,15 @@ with open('config.yaml', 'r', encoding='utf-8') as file:
 
 #stauth.Hasher.hash_passwords(config['credentials'])
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
+#authenticator = stauth.Authenticate(
+#    config['credentials'],
+#    config['cookie']['name'],
+#    config['cookie']['key'],
+#    config['cookie']['expiry_days']
+#)
 
-with open('config.yaml', 'w') as file:
-    yaml.dump(config, file, default_flow_style=False, allow_unicode=True)
+#with open('config.yaml', 'w') as file:
+#    yaml.dump(config, file, default_flow_style=False, allow_unicode=True)
 
 def login():
     try:
